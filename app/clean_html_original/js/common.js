a5ced6cdd9c1057cc796ef11f1f028e8b1b78d97
Vadym <vadosevich@gmail.com>
2016-12-02 13:59:23 +0200

    Header and parallax bg

--- app/clean_html_original/js/common.js
+++ app/clean_html_original/js/common.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
 
-	
-	
+	function heightDetect(){
+		$('.main_head').css('height', $(window).height());
+	};
+
+	heightDetect()
+
+	$(window).resize(function(){
+		heightDetect();
+	});
+
+	$(".toggle_menu, .menu_item").click(function() {
+	 	$(".sandwich").toggleClass("active");
+
+	 	if($(".top_menu").is(":visible")) {
+	 		$(".top_menu").fadeOut(600);
+	 	} else {
+	 		$(".top_menu").fadeIn(600);
+	 	}
+	 	
+	});
+
+
 });
+
+
+$(window).load(function() { 
+	$(".loader_inner").fadeOut(); 
+	$(".loader").delay(400).fadeOut("slow"); 
+});
+
