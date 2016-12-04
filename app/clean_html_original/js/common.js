519aaf8cd4c43427a757e0f04a6f1f265c62bb8d
Vadym <vadosevich@gmail.com>
2016-12-04 13:41:37 +0200

    Template for section

--- app/clean_html_original/js/common.js
+++ app/clean_html_original/js/common.js
@@ -10,17 +10,28 @@ $(document).ready(function() {
 		heightDetect();
 	});
 
-	$(".toggle_menu, .menu_item").click(function() {
+	$(".toggle_menu").click(function() {
 	 	$(".sandwich").toggleClass("active");
+	 });
 
+	$(".top_menu ul li a").click(function(){
+		$(".top_menu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+	}).append("<span>");
+
+	$(".toggle_menu").click(function() {
+	
 	 	if($(".top_menu").is(":visible")) {
+	 		$(".top_text").removeClass("h_opacity");
 	 		$(".top_menu").fadeOut(600);
+	 		$(".top_menu li a").removeClass("fadeInUp animated");
 	 	} else {
+	 		$(".top_text").addClass("h_opacity");
 	 		$(".top_menu").fadeIn(600);
+	 		$(".top_menu li a").addClass("fadeInUp animated");
 	 	}
 	 	
 	});
-
 
 });
 
