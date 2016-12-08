10f15b1e9126d2dcbd0cf26e35c4f628e56622e6
vadosevich <vadosevich@gmail.com>
2016-12-09 01:44:59 +0200

    Section Portfolio done

--- app/clean_html_original/js/common.js
+++ app/clean_html_original/js/common.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
 
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function(){
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
 	$(".popup").magnificPopup({ type: "image"});
+	$(".popup_content").magnificPopup({ type: "inline", midClick: true, showCloseBtn: true});
 
 	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
 	$(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
 
-	$(".animation_1").animated("flipInY", "fadeOutY");
-	$(".animation_2").animated("fadeInLeft", "fadeOutLeft");
-	$(".animation_3").animated("fadeInRight", "fadeOutRight");
+	$(".animation_1").animated("flipInY", "fadeOutDown");
+	$(".animation_2").animated("fadeInLeft", "fadeOutDown");
+	$(".animation_3").animated("fadeInRight", "fadeOutDown");
 
-	$(".left .resume_item").animated("fadeInLeft", "fadeOutLeft");
-	$(".right .resume_item").animated("fadeInRight", "fadeOutRight");
+	$(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
+	$(".right .resume_item").animated("fadeInRight", "fadeOutDown");
 
 
 	function heightDetect(){
@@ -44,6 +52,11 @@ $(document).ready(function() {
 	 		$(".top_menu li a").addClass("fadeInUp animated");
 	 	}
 	 	
+	});
+
+	$(".portfolio_item").each(function(i){
+		$(this).find("a").attr("href", "#work_" + i)	
+		$(this).find(".podrt_descr").attr("id", "work_" + i)	
 	});
 
 });
